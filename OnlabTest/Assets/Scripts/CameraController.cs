3a1093deb7b4f42065c874a3141912828f658d6f
Gabe <vecseigabor.x@gmail.com>
2016-03-09 23:52:18 +0100

    CameraController script elkészítése

--- OnlabTest/Assets/Scripts/CameraController.cs
+++ OnlabTest/Assets/Scripts/CameraController.cs
@@ -1,84 +1,98 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+
+[RequireComponent (typeof (Camera))]
+[RequireComponent (typeof (RectTransform))]
 
 public class CameraController : MonoBehaviour {
 
-	GameObject player_b;
-	GameObject player_r;
-	ArrayList players = new ArrayList();
+	public Transform[] targets;
+	public float boundingBoxPadding = 2f;
+	public float minimumOrthographicSize = 8f;
+	public float zoomSpeed = 20f;
+	Vector3 velocity = Vector3.zero;
+	public float smoothTime = 0.3f;
 
-	float minX;
-	float maxX;
-	float minY;
-	float maxY;
+	//Meg lehet adni, hogy a kamera meddig mozoghasson a térben
+	//Bizonyos határokat így nem tud átlépni
+	public bool minMaxValues = false;
+	public float maxX = Mathf.Infinity;
+	public float minX = 0;
+	public float maxY = Mathf.Infinity;
+	public float minY = 0;
 
-	Quaternion rot;
-	Vector3 pos;
-	public Vector3 angles;
+	Camera camera;
 
-	public float camSpeed;
-	public float camDist;
-
-	Vector3 finalLookAt;
-	Vector3 cameraBuffer;
-	float camSize;
-
-	void Start(){
-		
-		player_b = GameObject.Find ("Player_b");
-		player_r = GameObject.Find ("Player_r");
-		players.Add (player_b);
-		players.Add (player_r);
+	void Awake () 
+	{
+		camera = GetComponent<Camera>();
+		camera.orthographic = true;
+		targets [0] = GameObject.Find ("Player_b").GetComponent<Transform> ();
+		targets [1] = GameObject.Find ("Player_r").GetComponent<Transform> ();
 	}
 
-	void Update() {
-
-		CalculateBounds();
-		CalculateCameraPosAndSize();
-
+	void LateUpdate()
+	{
+		Rect boundingBox = CalculateTargetsBoundingBox();
+		//Beállítjuk folyton a kamera pozícióját
+		//De úgy hogy nem folyton változik hanem szépen odamegy valamennyi idő alatt amit megadhatunk
+		transform.position = Vector3.SmoothDamp(transform.position, CalculateCameraPosition(boundingBox), ref velocity, smoothTime);
+		//Beállítjuk folyton a kamera méretét
+		camera.orthographicSize = CalculateOrthographicSize(boundingBox);
 	}
 
-	void CalculateBounds(){
-		minX = Mathf.Infinity;
-		maxX = -Mathf.Infinity;
-		minY = Mathf.Infinity;
-		maxY = -Mathf.Infinity;
-	
-		foreach (GameObject player in players) {
-			Vector3 tempPlayer = player.transform.position;
-			//X Bounds
-			if (tempPlayer.x < minX)
-				minX = tempPlayer.x;
-			if (tempPlayer.x > maxX)
-				maxX = tempPlayer.x;
-			//Y Bounds
-			if (tempPlayer.y < minY)
-				minY = tempPlayer.y;
-			if (tempPlayer.y > maxY)
-				maxY = tempPlayer.y;
+	//Egy olyan "dobozt" készít amiben benne van az összes figyelt GameObject
+	//Itt például a két játékos
+	Rect CalculateTargetsBoundingBox()
+	{
+		float minX = Mathf.Infinity;
+		float maxX = Mathf.NegativeInfinity;
+		float minY = Mathf.Infinity;
+		float maxY = Mathf.NegativeInfinity;
+
+		foreach (Transform target in targets) {
+			Vector3 position = target.position;
+
+			minX = Mathf.Min(minX, position.x);
+			minY = Mathf.Min(minY, position.y);
+			maxX = Mathf.Max(maxX, position.x);
+			maxY = Mathf.Max(maxY, position.y);
 		}
+		return Rect.MinMaxRect(minX - boundingBoxPadding, maxY + boundingBoxPadding, maxX + boundingBoxPadding, minY - boundingBoxPadding);
 	}
 
-	void CalculateCameraPosAndSize() {
-		Vector3 cameraCenter = Vector3.zero;
+	//A camera pozícióját számolja ki, azaz annak közép pontját, hogy hol legyen
+	Vector3 CalculateCameraPosition(Rect boundingBox)
+	{
+		Vector2 boundingBoxCenter = boundingBox.center;
+		Vector3 newCameraPosition = new Vector3(boundingBoxCenter.x, boundingBoxCenter.y, camera.transform.position.z);
 
-		foreach(GameObject player in players){
-			cameraCenter += player.transform.position;
+		if (minMaxValues == true) {
+			//Nem léphet át bizonyos határokat a kamera pozíciója
+			if (newCameraPosition.x > maxX)
+				newCameraPosition.x = maxX;
+			if (newCameraPosition.x < minX)
+				newCameraPosition.x = minX;
+			if (newCameraPosition.y > maxY)
+				newCameraPosition.y = maxY;
+			if (newCameraPosition.y < minY)
+				newCameraPosition.y = minY;
 		}
 
-		Vector3 finalCameraCenter = cameraCenter / players.Count;
-		//Rotates and Positions camera around a point
-		rot = Quaternion.Euler(angles);
-		pos = rot * new Vector3(0f, 0f, -camDist) + finalCameraCenter; 
-		this.transform.rotation = rot;
-		this.transform.position = Vector3.Lerp(transform.position, pos, camSpeed * Time.deltaTime);
-		finalLookAt = Vector3.Lerp (finalLookAt, finalCameraCenter, camSpeed * Time.deltaTime);
-		this.transform.LookAt(finalLookAt);
-		//Size
-		float sizeX = maxX - minX + cameraBuffer.x;
-		float sizeY = maxY - minY + cameraBuffer.y;
-		camSize = (sizeX > sizeY ? sizeX : sizeY);
-		Camera.main.orthographicSize = camSize * 0.5f;
+		return newCameraPosition;
+	}
+
+	//A kamera ortografikus méretét számolja ki
+	float CalculateOrthographicSize(Rect boundingBox)
+	{
+		float orthographicSize = camera.orthographicSize;
+		Vector3 topRight = new Vector3(boundingBox.x + boundingBox.width, boundingBox.y, 0f);
+		Vector3 topRightAsViewport = camera.WorldToViewportPoint(topRight);
+
+		if (topRightAsViewport.x >= topRightAsViewport.y)
+			orthographicSize = Mathf.Abs(boundingBox.width) / camera.aspect / 2f;
+		else
+			orthographicSize = Mathf.Abs(boundingBox.height) / 2f;
+
+		return Mathf.Clamp(Mathf.Lerp(camera.orthographicSize, orthographicSize, Time.deltaTime * zoomSpeed), minimumOrthographicSize, Mathf.Infinity);
 	}
 }
