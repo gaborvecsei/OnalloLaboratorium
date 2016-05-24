8078b370c303b305d8e5060b281e5dcfa5b9e090
Gabe <vecseigabor.x@gmail.com>
2016-05-24 15:50:56 +0200

    Doksi update

--- OnlabTest/Assets/Scripts/EnergyBarController.cs
+++ OnlabTest/Assets/Scripts/EnergyBarController.cs
@@ -19,13 +19,14 @@ public class EnergyBarController: MonoBehaviour {
 		//Azért kell, hogy ugyan annyi legyen a max értéke mint a játékidőnek
 		gameTimeBar.maxValue = GameManager.gameTime;
 		energyBar_r.maxValue = GameManager.maxBallHoldTime;
+		player_r = GameObject.Find ("Player_r");
+		player_b = GameObject.Find ("Player_b");
 	}
 
 	//Mindkét játékosnál "lekérjük", az adatot hogy meddig volt a labda
 	//Aztán azt megjelenítjük a Slider-en
 	void Update(){
-		player_r = GameObject.Find ("Player_r");
-		player_b = GameObject.Find ("Player_b");
+		
 		energyBar_r.value = player_r.GetComponent<PlayerController> ().BallHoldTime;
 		energyBar_b.value = player_b.GetComponent<PlayerController> ().BallHoldTime;
 		int gameTimeValue = GameManager.gameTime;
